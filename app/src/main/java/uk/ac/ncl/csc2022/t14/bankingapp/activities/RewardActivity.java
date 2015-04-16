3afd8595b2cdd697ed50f67c0497405dc37cd8e4
Sam Liddle <samliddleg@gmail.com>
2015-04-17 00:09:32 +0100

    Removed overflow from action bar on Reward Activity

--- app/src/main/java/uk/ac/ncl/csc2022/t14/bankingapp/activities/RewardActivity.java
+++ app/src/main/java/uk/ac/ncl/csc2022/t14/bankingapp/activities/RewardActivity.java
@@ -63,21 +63,6 @@ public class RewardActivity extends LloydsActionBarActivity implements ChooseRew
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void chooseRewardPassed() {
 
         // Activity finishes upon successful reward retrieval
